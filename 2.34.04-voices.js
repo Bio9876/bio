07f9b0d4ce39c3e265f35c1d3d3bff7ea91be1e5
Bio9876 <131152089+Bio9876@users.noreply.github.com>
2023-04-18 15:06:31 +0530

    Add files via upload

---
+++ 2.34.04-voices.js
@@ -0,0 +1,9 @@
+<div class="column">
+  <h1>Your request was blocked due to network reputation controls</h1>
+  <h2>Here is how to access Narakeet:</h2>
+  <p><strong>Are you using a VPN or a Proxy service?</strong> Some other user of the same service might have tried to abuse our system recently. Try switching to a different endpoint or turning off the VPN/Proxy when accessing Narakeet</p>
+  <p><strong>Are you using a browser extension that anonymizes or modifies requests?</strong> The controls might be too strict, and your extension makes the request indistingushable from a web scraping bot. Change your extension settings, whitelist Narakeet, or turn it off temporarily when accessing our service.</p>
+  <p><strong>Are you accessing via a public network?</strong> If you are accessing Narakeet from a public place shared by lots of other users (airport wifi, coffeeshop wifi...), your request is probably blocked because someone else tried to abuse our services from the same network recently. Try switching to a different network (eg switch to your mobile access point), or accessing the system from some other place later.</p>
+  <h2>Need help?</h2>
+  <p>Please get in touch with <a href="mailto:contact@narakeet.com">contact@narakeet.com</a>.</p>
+</div>
